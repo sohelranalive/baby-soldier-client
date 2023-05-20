1b20f8a6c936f9b5be7ef144e571e6f7c363e6a4
sohelranalive <sohel.cse37@gmail.com>
2023-05-21 00:19:23 +0600

    show tittle according to pathname added

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -7,11 +7,60 @@ const Main = () => {
 
     const { pathname } = useLocation();
 
+
     // Automatically scrolls to top whenever pathname changes
     useEffect(() => {
         window.scrollTo({ top: 0, behavior: 'smooth' });
     }, [pathname]);
 
+    useEffect(() => {
+        const pageTitle = getTitleFromPathName(pathname);
+        document.title = pageTitle;
+    }, [pathname]);
+
+    console.log(pathname);
+
+    const getTitleFromPathName = (pathName) => {
+        let pageTitle = '';
+
+        switch (pathName) {
+            case '/':
+                pageTitle = 'Baby Soldier';
+                break;
+            case '/register':
+                pageTitle = 'BS | Register';
+                break;
+            case '/login':
+                pageTitle = 'BS | Login';
+                break;
+            case '/blog':
+                pageTitle = 'BS | Blog';
+                break;
+            case '/addToys':
+                pageTitle = 'BS | Add Toy';
+                break;
+            case '/allToys':
+                pageTitle = 'BS | All Toy';
+                break;
+            case '/myToys':
+                pageTitle = 'BS | My Toys';
+                break;
+            case 'toyDetails/':
+                pageTitle = 'BS | Toy Details';
+                break;
+
+            default:
+                if (pathName.startsWith('/toyDetails/')) {
+                    const toyId = pathName.split('/toyDetails/')[1];
+                    pageTitle = `Toy Details - ${toyId.slice(0, 5)}...`;
+                } else {
+                    pageTitle = 'Baby Soldier';
+                }
+                break;
+        }
+
+        return pageTitle;
+    };
 
     return (
         <div>
